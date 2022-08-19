2157db89eacb7c9317658133a9443afee2992f18
barnescaleb <barnescaleb119@gmail.com>
2022-08-19 11:50:19 -0700

    Add app.js & index.html

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-const choose = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll('input')
 
-function getComputerChoice(choose) {
-    return choose[Math.floor(Math.random() * choose.length)];
+function getComputerChoice() {
+        let choose = ['Rock', 'Paper', 'Scissors'];
+        return choose[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice(choose));
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice()
+    let result = ""
+    
+    
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+
+        playerScore += 1
+        result = ('You Win! ' + playerSelection + ' beats ' + computerSelection
+        + '<br><br>Player Score: ' + playerScore + '<br><br>Computer Score: ' + computerScore)
+
+        if (playerScore == 5) {
+            result += 'You Won!'
+        }
+
+    } 
+    else if (playerSelection == computerSelection) {
+        result = ('Tie! ' 
+        + '<br><br>Player Score: ' + playerScore + '<br><br>Computer Score: ' + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('Computer Wins! ' + computerSelection + ' beats ' + playerSelection
+        + '<br><br>Player Score: ' + playerScore + '<br><br>Computer Score: ' + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>You Lose!'
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
+
+};
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+        })
+    });
+
